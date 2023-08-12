8ce60cf33c36e32b9971be7e3f49fc11a2166772
diogocorrea <diogocorrea@id.uff.br>
2023-08-12 10:27:46 -0300

    specialty init

--- src/libs/routes.ts
+++ src/libs/routes.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from "fastify";
 import { doctorsRoutes } from "../modules/doctors/doctors.routes";
+import { specialtyRoutes } from "../modules/specialties/specialties.routes";
 
 export async function appRoutes(app: FastifyInstance) {
-    app.register(doctorsRoutes, { prefix: '/doctors' })
+    app.register(doctorsRoutes, { prefix: '/doctors' });
+    app.register(specialtyRoutes, { prefix: '/specialties' });
 }
